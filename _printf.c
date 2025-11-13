2ee6e14871d6af0d36586e424a6f1a40f42889ce
Emilien-Marot <12394@holbertonstudents.com>
2025-11-13 06:40:05 +0000

    exercise 1.1.1

--- _printf.c
+++ _printf.c
@@ -1,13 +1,51 @@
 #include "main.h"
 #include <stdarg.h>
 
+int _print_s(char *s)
+{
+	int i, len = 0;
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+		len++;
+	}
+	return(len);
+}
+
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, str_len;
 
-	va_start(format, args);
+	va_start(args, format);
 	
-	write(1, &c, 1);
+	for(i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if(format[i] == '%')
+			{
+				_putchar('%');
+				str_len++;
+			}
+			else if(format[i] == 'c')
+			{
+				_putchar(va_arg(args, char));
+				str_len++;
+			}
+			else if(format[i] == 's')
+			{
+				str_len += _print_s(va_arg(args, const char*));
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			str_len++;
+		}
+	}
 	va_end(args);
+	return(str_len);
 }
--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
 	int i;
-	char *test_string = "%d\n";
+	char *test_string = "%d %d\n";
 	printf(test_string, 1);
 	for(i = 0; test_string[i] != '\0'; i++)
-		putchar(test_string[i]);
+		write(1, &test_string[i], 1);
 	return(0);
 }
